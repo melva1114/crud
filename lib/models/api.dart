2e578760b1649e2cd6487b75840db25441483a68
melva1114 <151824347+melva1114@users.noreply.github.com>
2025-02-26 08:55:30 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl {
+  static String tambah = "http://192.168.231.43/biodata/create.php";
+  static String lihat = "http://192.168.231.43/biodata/details.php";
+  static String edit = "http://192.168.231.43/biodata/update.php";
+  static String hapus = "http://192.168.231.43/biodata/delete.php";
+  static String data = "http://192.168.231.43/biodata/list.php";
+}
